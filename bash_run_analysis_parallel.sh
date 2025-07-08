a11a2b71edfb2e20379315ab51d52cdbb65270a9
Aziz <abdoulaziz97diallo@gmail.com>
2025-07-08 17:30:04 +0200

    Refactoring: bash file for parallel running

--- bash_run_analysis_parallel.sh
+++ bash_run_analysis_parallel.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+k_dim=1
+methods="Newton_orbit"
 #PBS -S /bin/bash
 #PBS -N analysis_run
 #PBS -M diallo
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1:ppn=$k_dim
 #PBS -l walltime=0:03:00
-#PBS -t 0-3
+# #PBS -t 0-3 #To subimit the job as an array job, uncomment this line and set the range according to your needs
 #PBS -m bea
 #PBS -o logs/test.out
 #PBS -e logs/error_test.err
@@ -18,15 +20,15 @@ module load anaconda3/2024.06-1
 #module load python3.12
 
 # Checking for the virtual environment directory
-#VENV_DIR=$HOME/Orbit-Solutions-/.venv
+#VENV_DIR=$HOME/Orbit-Solutions-/.myvenv
 cd $PBS_O_WORKDIR
 mkdir -p logs
 echo "PBS_ARRAY_INDEX = ${PBS_ARRAY_INDEX}"
 #Create venv if it doesn't exist
-if [ ! -d "$HOME/Orbit-Solutions-/.venv" ]; then
+if [ ! -d "$HOME/Orbit-Solutions-/.myvenv" ]; then
     echo " Creating the virtual environment..."
-    python3.12 -m venv $HOME/Orbit-Solutions-/.venv
-    source $HOME/Orbit-Solutions-/.venv/bin/activate
+    python3.12 -m venv $HOME/Orbit-Solutions-/.myvenv
+    source $HOME/Orbit-Solutions-/.myvenv/bin/activate
     echo " Installing dependencies..."
     if [ ! -d $HOME/Orbit-Solutions-/requirements.txt ]; then
         echo "You need to provide a requirements txt file containing the dependencies"
@@ -36,15 +38,11 @@ if [ ! -d "$HOME/Orbit-Solutions-/.venv" ]; then
     fi
 else
     echo "Using existing environment"
-    source $HOME/Orbit-Solutions-/.venv/bin/activate
+    source $HOME/Orbit-Solutions-/.myvenv/bin/activate
 fi
 
-N_z=(16 32)
-methods=("Newton_Picard_sub_proj" "Newton_orbit")
+
 #splitting the array index to run the two programs Newton and Newton-Picard
-dim_index=$((PBS_ARRAY_INDEX / 2))
-method_index=$((PBS_ARRAY_INDEX % 2))
-n_z=${N_z[$dim_index]}
-method=${methods[$method_index]}
-echo "Running with n_z: $n_z and method: $method"
-python3.12 run_analysis.py -n_z=$n_z -method=$method -sparse_jac=True
+
+echo "Running with method: $methods"
+python3.12 run_analysis.py -k_dim=$k_dim -method=$method
