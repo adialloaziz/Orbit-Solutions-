Fixing error with the jacobian computation. The multiplication of the covariance matrix I by the parameter I wa omitted. Begin the implementation of the continuation algorithm pseudo arclength parametrization. Implementation of Mckean-Vlasov with the Kuramoto Kernel and periodic BCs.

models.py
@@ -260,6 +260,7 @@ class Mckean_Vlasov:
         self.read_params()
         self.mesh1D = self.mesh_1D()  # Initialize the mesh
         self.C_mat =self.Conv_mat()  # Precompute the convolution matrix for the Haissinski kernel
+        self.C_mat_kuramoto = self.Conv_mat_kuramoto() #Precompute the convolution matrix for the Kuramoto potential
         
     def update_params(self, **kwargs):
         for key, value in kwargs.items():
@@ -271,6 +272,8 @@ class Mckean_Vlasov:
         if 'n_z' in kwargs:
             self.mesh1D = self.mesh_1D()  # Update the mesh if n_z changes
             self.C_mat = self.Conv_mat()  # Update the convolution matrix if n_z changes
+            self.C_mat_kuramoto = self.Conv_mat_kuramoto() #Update the convolution matrix for the Kuramoto potential if n_z changes
+
         
 
     def read_params(self): 
@@ -332,6 +335,8 @@ class Mckean_Vlasov:
                         self.alpha = float(res)
                     elif var == 'beta': #artificial parameter
                         self.beta = float(res)
+                    elif var == 'alpha_shift': #Shift parameter for the Kuramoto potential
+                        self.alpha_shift = float(res)
                     else:
                         raise ValueError(f"Unknown parameter: {var}")
                     
@@ -360,29 +365,33 @@ class Mckean_Vlasov:
         # y = np.where(z<1e-8,0, 2*(np.cosh(5*np.asinh(z)/3) - np.cosh(np.asinh(z)))/denom)
         return np.where(z<1e-5,0, 2*(np.cosh(5*np.asinh(z)/3) - np.cosh(np.asinh(z)))/denom)
     
-    # def kuramoto_potential(self, z):
+    def kuramoto_potential(self, z):
 
-        # return np.cos(z - self.alpha_shift)
+        return -np.cos(z - self.alpha_shift)
     
     def Conv_mat(self):
         "The convolution matrix for the Haissinski kernel"
         _,z, h = self.mesh1D  # Get the mesh and centers
         #Convolution matrix
         return h*sp.linalg.convolution_matrix(self.Haissinski_kernel(z), len(z), mode='same') 
-
-        # C_mat = np.zeros((len(z), len(z))) # Initialize a matrix to hold the kernel values
-        # for i in range(len(z)):
-        #     C_mat[i,:] = self.Haissinski_kernel(z[i] - z)
-        # return C_mat
+    def Conv_mat_kuramoto(self):
+        "The convolution matrix for the Kuramoto potential"
+        _,z, h = self.mesh1D  # Get the mesh and centers
+        #Convolution matrix
+        return h*sp.linalg.convolution_matrix(self.kuramoto_potential(z), len(z), mode='same')
+        
     
     def V(self, z, rho):
         "The potential function"
         _,_, h = self.mesh1D
         # return  z*z/2 + self.I*(self.C_mat @ rho)  # Convolve the kernel with rho using matrix multiplication
         # return np.ones_like(z)
-        # y = rho[:,0] if rho.ndim > 1 else rho
         return z*z/2 + h*self.I*sp.signal.convolve(self.Haissinski_kernel(z), rho, mode='same', method='fft')
         # return z*z/2 + np.trapezoid(self.Haissinski_kernel(z) * rho)
+    def V_kuramoto(self, z, rho):
+        "The potential function with the Kuramoto kernel"
+        _,_, h = self.mesh1D
+        return h*self.I*sp.signal.convolve(self.kuramoto_potential(z), rho, mode='same', method='fft')
 
     def mesh_1D(self):
         "Create a uniform mesh in the interval [xmin, xmax] with n_z points"
@@ -403,92 +412,51 @@ class Mckean_Vlasov:
         F_L = np.zeros_like(x_centers)  # Containining all the flux values
         #No flux on the boundaries
         F_L[1:] = B_m*rho[1:] - B_p*rho[:-1]
-        return self.D*F_L/(h*h)  # Flux of the density rho divided by h^2
+        return self.D*F_L/(h*h) 
+    
+    def flux_per(self,rho):
+        "Compute the flux of the density rho"
+        #We suppose a uniform mesh in the interval [xmin, xmax]
+        _, x_centers, h = self.mesh1D
+        V = self.V_kuramoto(x_centers, rho)  # Potential at the center of the cells
+
+        V_diff = np.zeros_like(x_centers)
+        V_diff[0] = V[0] - V[-1]
+        V_diff[1:] = np.diff(V)
+           
+        B_m = self.Bernoulli(-V_diff/self.D)
+        B_p = self.Bernoulli(V_diff/self.D)
+        F_L = np.zeros_like(x_centers)  # Containining all the flux values
+        #Periodic BCs
+        F_L[0] = B_m[0]*rho[0] - B_p[0]*rho[-1]
+        #Inner points
+        F_L[1:] = B_m[1:]*rho[1:] - B_p[1:]*rho[:-1]
+
+        return self.D*F_L/(h*h)
 
     
-    def dydt_scal(self, t, rho):
-        "Right hand side of the discretized(Finite Volume scheme) Mckean-Vlasov equation with time scaling"
-
-        return np.append(self.dydt(t, rho[:-1]) * rho[-1],0)
-    
-    def jacobian_scal(self, t, rho):
-        """Jacobian of the right hand side of the Mckean-Vlasov equation with time scaling"""
-        J = self.jacobian(t, rho[:-1])
-        n = self.n_z - 1
-        #Append the last row and column for the time scaling
-        J_scal = np.zeros((n+1, n+1))
-        J_scal[:n,:n] = J * rho[-1]
-        J_scal[:n,-1] = self.dydt(t, rho[:-1])
-        return J_scal
 
     def dydt(self, t, rho):
         "Right hand side of the discretized(Finite Volume scheme) Mckean-Vlasov equation"
         # All parameters are fixed here 
 
         F_L = self.flux(rho)  # Flux of the density y
-
         # dydt = 1/(h*h) * (F_K- F_L)  # Finite Volume scheme
         # F_K = np.roll(F_L, shift=-1)
-        dydt = (np.roll(F_L, shift=-1) - F_L) # Sifting matrix to apply the finite volume scheme
+        dydt = (np.roll(F_L, shift=-1) - F_L)
         return dydt
     
-    def dydt_1(self, t, rho):
-        "rhs of the discretized Mckean-Vlasov equation"
-        #We let the intensity of the interaction as a variable
-        return np.append(self.dydt(t, rho[:-1]),0)
-    def jacobian_1(self, t, rho):
-        """Jacobian of the right hand side of the Mckean-Vlasov equation"""
-        J = self.jacobian(t, rho[:-1])
-        n = self.n_z - 1
-        #Append the last row and column for the intensity of the interaction
-        J_1 = np.zeros((n+1, n+1))
-        J_1[:n,:n] = J
-        J_1[:n,-1] = self.C_mat @ rho[:-1]
-        return J_1
-
-
-    def dydt_dissip(self, t, rho):
-        "rhs of the discretized dissipative Mckean-Vlasov equation"
-        #Augmented rho with the unfolding parameter alpha
-        #T= rho[-2], alpha = rho[-1]
-        
-        T=self.T_ini
-        alpha=self.alpha
-        grad_H = np.ones_like(rho) #Gradient of the fisrt integral function: The mass
-
-        return T*(self.dydt(t,rho) - alpha*grad_H)
     
-    def dydt_dissip2(self, t, rho,m0=1):
-        "rhs of the discretized dissipative Mckean-Vlasov equation"
-        #Augmented rho with the time scaling variable and the unfolding parameter alpha
-        #T= rho[-2], alpha = rho[-1]
-        n = self.n_z - 1
-        grad_H = np.ones(n)
-
-        return np.append(self.dydt(t, rho[:-1]) - rho[-1]*grad_H, grad_H@rho[:-1]-self.m0)
-    
-    def jacobian_dissip2(self, t, rho):
-        """Jacobian of the right hand side of the dissipative Mckean-Vlasov equation"""
-        J = self.jacobian(t, rho[:-1])
-        n = self.n_z - 1
-        grad_H = np.ones(n)
-        #Append the last row and column for the unfolding parameter
-        J_dissip = np.zeros((n+1, n+1))
-        J_dissip[:n,:n] = J * rho[-1]
-        J_dissip[:n,-1] = -grad_H
-        J_dissip[-1,:n] = grad_H
-
-        return J_dissip
-
-    def jacobian_dissip(self, t, rho):
-        """Jacobian of the right hand side of the dissipative Mckean-Vlasov equation"""
-        # 
-        T = self.T_ini
-        
-        return T*self.jacobian(t, rho)
+     
+    def dydt_per(self,t,rho):
+        F_L = self.flux_per(rho)  # Flux of the density y
+        # dydt = 1/(h*h) * (F_K- F_L)  # Finite Volume scheme
+        # F_K = np.roll(F_L, shift=-1)
+        dydt = (np.roll(F_L, shift=-1) - F_L)
+        return dydt
     
     def jacobian(self, t, rho):
-        """Jacobian of the right hand side of the Mckean-Vlasov equation"""
+        """Jacobian wrt rho of the right hand side of the Mckean-Vlasov equation"""
         _, x_centers, h = self.mesh1D
         n = self.n_z - 1
         h_sq = h * h
@@ -521,66 +489,108 @@ class Mckean_Vlasov:
              
         
         J_non_lin = np.zeros((self.n_z-1, self.n_z-1))  # Initialize the Jacobian matrix
-        J_non_lin[0,:] = -(B_prime_m[0]*rho[1] + B_prime_p[0]*rho[0])*(self.C_mat[1,:] - self.C_mat[0,:])
+        J_non_lin[0,:] = -(B_prime_m[0]*rho[1] + B_prime_p[0]*rho[0])*self.I*(self.C_mat[1,:] - self.C_mat[0,:])
 
-        J_non_lin[-1,:] = (B_prime_m[-1]*rho[-1] + B_prime_p[-1]*rho[-2])*(self.C_mat[-1,:] - self.C_mat[-2,:])
+        J_non_lin[-1,:] = (B_prime_m[-1]*rho[-1] + B_prime_p[-1]*rho[-2])*self.I*(self.C_mat[-1,:] - self.C_mat[-2,:])
         
         for i in range(1,self.n_z-2):
-            flux_p = -(B_prime_m[i]*rho[i+1] + B_prime_p[i]*rho[i])*(self.C_mat[i+1,:] - self.C_mat[i,:])
+            flux_p = -(B_prime_m[i]*rho[i+1] + B_prime_p[i]*rho[i])*self.I*(self.C_mat[i+1,:] - self.C_mat[i,:])
 
-            flux_m = (B_prime_m[i-1]*rho[i] + B_prime_p[i-1]*rho[i-1])*(self.C_mat[i,:] - self.C_mat[i-1,:])
+            flux_m = (B_prime_m[i-1]*rho[i] + B_prime_p[i-1]*rho[i-1])*self.I*(self.C_mat[i,:] - self.C_mat[i-1,:])
 
 
             J_non_lin[i,:] = flux_p + flux_m
 
         return np.asarray((J_linear +J_non_lin))/h_sq
     
-
-    def jacobian_optim(self, t, rho):
-        """Jacobian of the right hand side of the Mckean-Vlasov equation"""
+    def jacobian_per(self, t ,rho):
+        """Jacobian wrt rho of the right hand side of the Mckean-Vlasov equation"""
         _, x_centers, h = self.mesh1D
         n = self.n_z - 1
         h_sq = h * h
 
         # Compute potential and differences
-        V = self.V(x_centers, rho)
-        V_diff = np.diff(V)
+        V = self.V_kuramoto(x_centers, rho)
+        V_diff = np.zeros_like(x_centers)
+        V_diff[0] = V[0] - V[-1]
+        V_diff[1:] = np.diff(V)
+        
 
         # Compute Bernoulli functions
-        B_m = self.Bernoulli(-V_diff)
-        B_p = self.Bernoulli(V_diff)
+        B_m = self.Bernoulli(-V_diff/self.D)
+        B_p = self.Bernoulli(V_diff/self.D)
 
         # Build linear part diagonal elements
-        diag_J = np.empty(n)
-        diag_J[0] = -B_p[0]
-        diag_J[1:-1] = -(B_m[:-1] + B_p[1:])
-        diag_J[-1] = -B_m[-1]
+        diag_J = np.zeros(n)
+        diag_J[0] = -B_p[1] - B_m[0]
+        diag_J[1:-1] = -(B_m[1:-1] + B_p[2:])
+        diag_J[-1] = -B_m[-1] - B_p[0]
 
-        # Create sparse linear Jacobian
+        # Create linear part sparse of the Jacobian
         J_linear = sp.sparse.diags(
-            [B_p, diag_J, B_m],
+            [B_p[1:], diag_J, B_m[1:]],
             [-1, 0, 1],
             shape=(n, n),
             format='csr'
         )
 
-        # Compute nonlinear flux contribution
-        B_prime_m = self.derivative_Bernoulli(-V_diff)
-        B_prime_p = self.derivative_Bernoulli(V_diff)
+        J_linear[0,-1] = B_p[0]
+        J_linear[-1,0] = B_m[0]
 
-        # Compute convolution difference efficiently
-        Convol_dif = np.diff(self.C_mat, axis=0, prepend=0)
+        # Compute nonlinear flux contribution
+        B_prime_m = self.derivative_Bernoulli(-V_diff/self.D)
+        B_prime_p = self.derivative_Bernoulli(V_diff/self.D)
+             
         
-        # Compute diagonal for flux derivatives
-        temp = B_prime_m * rho[1:] + B_prime_p * rho[:-1]
-        temp = np.append(temp, 0)
+        J_non_lin = np.zeros((self.n_z-1, self.n_z-1))  # Initialize the Jacobian matrix
+        # J_non_lin[0,:] = -(B_prime_m[0]*rho[1] + B_prime_p[0]*rho[0])*(self.C_mat[1,:] - self.C_mat[0,:])
+
+        # J_non_lin[-1,:] = (B_prime_m[-1]*rho[-1] + B_prime_p[-1]*rho[-2])*(self.C_mat[-1,:] - self.C_mat[-2,:])
         
-        # Apply diagonal and compute flux derivatives
-        flux_p_deriv = -temp[:, np.newaxis] * Convol_dif
-        flux_m_deriv = np.roll(flux_p_deriv, shift=1, axis=0)
+        flux_p = -(B_prime_m[1]*rho[1] + B_prime_p[1]*rho[0])*self.I*(self.C_mat_kuramoto[1,:] - self.C_mat_kuramoto[0,:])
+        flux_m = -(B_prime_m[0]*rho[0] + B_prime_p[0]*rho[-1])*self.I*(self.C_mat_kuramoto[-1,:] - self.C_mat_kuramoto[0,:])
+        J_non_lin[0,:] = flux_p + flux_m
+
+        flux_p = (B_prime_m[0]*rho[0] + B_prime_p[0]*rho[-1])*self.I*(self.C_mat_kuramoto[-1,:] - self.C_mat_kuramoto[0,:])
+        flux_m = (B_prime_m[-1]*rho[-1] + B_prime_p[-1]*rho[-2])*self.I*(self.C_mat_kuramoto[-1,:] - self.C_mat_kuramoto[-2,:])
+        J_non_lin[-1,:] = flux_p + flux_m
+
+        for i in range(1,self.n_z-2):
+            flux_p = -(B_prime_m[i+1]*rho[i+1] + B_prime_p[i+1]*rho[i])*self.I*(self.C_mat_kuramoto[i+1,:] - self.C_mat_kuramoto[i,:])
+
+            flux_m = (B_prime_m[i]*rho[i] + B_prime_p[i]*rho[i-1])*self.I*(self.C_mat_kuramoto[i,:] - self.C_mat_kuramoto[i-1,:])
+
+
+            J_non_lin[i,:] = flux_p + flux_m
+
+        return np.asarray((J_linear + J_non_lin))/h_sq
+   
+    def df_dI_per(self, t, rho):
+        "Derivative of the right hand side of the Mckean-Vlasov equation with respect to the interaction intensity I"
+        _, x_centers, h = self.mesh1D
+        V = self.V_kuramoto(x_centers, rho)  # Potential at the center of the cells
+        V_diff = np.zeros_like(x_centers)
+        V_diff[0] = V[0] - V[-1]
+        V_diff[1:] = np.diff(V)
+        # B_m = self.Bernoulli(-V_diff/self.D)
+        # B_p = self.Bernoulli(V_diff/self.D)
+        dV_dI = h*sp.signal.convolve(self.Haissinski_kernel(x_centers), rho, mode='same', method='fft')
+        # print("Shape of dV_dI:", dV_dI.shape)
+        B_prime_m = self.derivative_Bernoulli(-V_diff/self.D)
+        B_prime_p = self.derivative_Bernoulli(V_diff/self.D)
+        print("Shape of B_prime_m:", B_prime_m.shape)
+        dF_L_dI = np.zeros_like(x_centers)
+        #Periodic BCs
+        dF_L_dI[0] = -(B_prime_m[1]*rho[1] + B_prime_p[1]*rho[0])*(dV_dI[1] - dV_dI[0]) + (B_prime_m[0]*rho[0] + B_prime_p[0]*rho[-1])*(dV_dI[0] - dV_dI[-1])
+        dF_L_dI[-1] = (B_prime_m[0]*rho[0] + B_prime_p[0]*rho[-1])*(dV_dI[-1] - dV_dI[0]) + (B_prime_m[-1]*rho[-1] + B_prime_p[-1]*rho[-2])*(dV_dI[-1] - dV_dI[-2])
+
+        #Inner points
+        flux_p = -(B_prime_m[2:]*rho[2:] + B_prime_p[2:]*rho[1:-1])*(dV_dI[2:] - dV_dI[1:-1])
+        flux_m = (B_prime_m[1:-1]*rho[1:-1] + B_prime_p[1:-1]*rho[:-2])*(dV_dI[1:-1] - dV_dI[:-2])
         
-        # Combine components
-        return np.asarray((J_linear.to_array() + flux_p_deriv - flux_m_deriv)) / h_sq
+        dF_L_dI[1:-1] =  flux_p + flux_m
+
+        return dF_L_dI/(h*h)
 
 
     def dydt_new(self,t, rho):

test_Mckean_Vlasov.py
@@ -88,6 +88,7 @@ if __name__ == "__main__":
     # I_values = np.linspace(1.0, 1.2, 15)  # Example intensity values
     T = model.T_ini
     I_max = 1.026
+    I_min = 0.97
     cont_step = 0.013
 
     solutions = []
@@ -101,9 +102,10 @@ if __name__ == "__main__":
     file = results_dir / f"branch_solutions_{today}.txt"
     file_pkl = results_dir / f"branch_solutions_{today}.pkl"
 
-    with open(file, "w") as f:
-        f.write("I_value\tTstar\tystar\n")
-        while ((model.I <= I_max) and (cont_step > 1e-4)):
+    with open(file, "w") as fic:
+        fic.write("I_value\tTstar\tystar\n")
+        # while ((model.I <= I_max) and (cont_step > 1e-4)):
+        while (model.I > I_min) and (cont_step > 1e-4):
             print(f"Computing solution for Intensity I = {model.I}")
             k, T_by_iter, y_by_iter, Norm_B, Abs_Err, Rel_Err, converged, mass = run(model,f, J, model.n_z,"Newton_mass_conserv4",
                                                                                 model.p0,y0=y0,T=T, filename=None)            
@@ -111,7 +113,8 @@ if __name__ == "__main__":
             if converged == -1:
                 print("Branch continuation stopped due to divergence.")
                 cont_step /= 2  # Reduce the continuation step
-                model.I -= cont_step  # Step back
+                # model.I -= cont_step  # Step back
+                model.I += cont_step
                 continue  # Retry with a smaller step
             else:
                 if k <= 3:
@@ -121,11 +124,11 @@ if __name__ == "__main__":
                 y0 = y_by_iter[k]  # Update y0 for the next iteration
                 solutions.append((model.I, y0, T, mass[k],k))
 
-                model.I += cont_step  # Increment the Intensity for the next step
-            
+                # model.I += cont_step  # Increment the Intensity for the next step
+                model.I -= cont_step #Step back as we go down the branch
 
                 # Save intermediate results
-                f.write(f"{model.I}\t{T_by_iter[k]}\t{y_by_iter[k].tolist()}\n")
+                fic.write(f"{model.I}\t{T_by_iter[k]}\t{y_by_iter[k].tolist()}\n")
                 print("#-------------------------------------------------------------# \n")
 	    
                 # Save after each successful computation

utility.py
@@ -323,8 +323,8 @@ class orbit:
         # Phase condition
         s = (y + Delta_q_r - y_tild) @ self.f(T, y_tild) #Taylor approx of the rhs 
         ds_dy = self.f(T, y_tild) #Derivative wrt y
-        ds_dT = (y - y_tild)@(unscaled_f(T,y_tild) - alpha*H) #Derivative wrt T
-        ds_dalpha = -T_star*(y - y_tild)@(H) #Derivative wrt alpha
+        ds_dT = (y - y_tild)@unscaled_f(T,y_tild) # - alpha*H) #Derivative wrt T
+        ds_dalpha = (y - y_tild)@(H) #-T_star*(y - y_tild)@(H) #Derivative wrt alpha
 
         # Mass conservation condition
         #Delta_m = H @ y - m0
@@ -334,7 +334,7 @@ class orbit:
         dm_dalpha = 0.0 #derivative wrt alpha
 
         # Periodicity condition
-        dr_dT = unscaled_f(T,y) - alpha*H #self.f(T,y)
+        dr_dT = unscaled_f(T,y) + alpha*H #self.f(T,y)
         # _, dr_dalpha = self.integ_sensitivity(y, np.zeros(self.dim), T, f_param = -T*H)
         # Build augmented linear system [A | b]
         top = np.hstack((Sp - Ip, (Vp.T@dr_dT).reshape(-1, 1), (Vp.T@(dr_dalpha+M_Delta_q_alpha)).reshape(-1, 1)))
@@ -345,7 +345,7 @@ class orbit:
         # Right-hand side (Taylor approx)
         
         sol = self.ode_solver(fun=self.f, t_span=[0.0, T], y0=y + Delta_q_r,
-                              t_eval=[T], method="BDF",
+                              t_eval=[T], method="RK45",
                               jac=self.Jacf,
                               rtol=1e-7, atol=1e-9)
         
@@ -579,85 +579,6 @@ class orbit:
 
         return k, T_by_iter, y_by_iter, Norm_B, Abs_Err, Rel_Err, converged, mass
     
-    def Newton_mass_conserv(self,y0,T_0, Max_iter, epsilon,h=1):
-        #h is the spatial step size
-        #________________________________INITIALISATION_________________________________
-        y_star, y_prev, T_star = y0.copy(), y0.copy(), T_0
-
-        y_by_iter, T_by_iter = np.zeros((Max_iter,self.dim)),np.zeros((Max_iter))
-        Norm_B, Abs_Err = np.zeros((Max_iter)), np.zeros((Max_iter))
-        mass = np.zeros((Max_iter))
-        Rel_Err = np.zeros((Max_iter))
-        I = np.eye(self.dim)
-        H = h*np.ones_like(y_star)
-        m0 = H @ y0
-        #______________________________Newton iteration loop________________
-        for k in range(Max_iter): # Stop criterion: norm_delta_y/norm_y0: To be kept in mind for small value of y 
-            
-            #Soving the whole system over one period
-            phi_T, monodromy = self.integ_monodromy(y_star,I,T_star)
-
-        #Selecting the phase-condition
-        #The orthogonality phase condition is imposed
-            d = 0
-            # c = self.f(T_star,y_prev)
-            c = self.f(T_star,y0)
-            s = (y_star - y0) @ self.f(T_star,y0)
-            # s = (y_star - y_prev)@self.f(T_star,y_prev)
-            bb = self.f(T_star, phi_T)
-
-            #Mass conservation condition
-            m = H @ (y_star - y0)
-            #c2 = H #derivative wrt y
-            d22 = H @ (self.f(T_star,y_star) - self.f(T_star,self.y0)) #0 #derivative wrt T
-
-
-
-            #Concat the whole matrix
-            top = np.hstack((monodromy - I, bb.reshape(-1,1)))  # Horizontal stacking of A11=M-I and A12=b
-            #
-            middle = np.hstack((c.reshape(1,-1),np.array([[d]])))  # Horizontal stacking of A21=c and A22=d
-            
-            #Mass conservation condition
-            #H.Jacf with H = [1,1,...,1] 
-            # H = h*np.ones((1,self.dim))
-            
-            # A31 = H @ I
-            bottom = np.hstack(((H.T).reshape(1,-1) ,np.array([[d22]]))) #Horizontal stacking of A31=H.Jacf and A32=0
-            Mat = np.vstack((top, middle,bottom))  # Vertical stacking of the three rows
-
-            #Right hand side concatenation
-            B = np.concatenate((phi_T - y_star, np.array([s]), np.array([m])))   #np.array([s(T_star,y_star)])))
-            
-            
-            XX, residues,rank,sing_val = lstsq(Mat,-B,lapack_driver='gelss') #Contain Delta_X and Delta_T
-            Delta_y = XX[:self.dim]
-            Delta_T = XX[-1]
-            
-            #Updating
-            y_prev = y_star
-            y_star += Delta_y
-            T_star += Delta_T
-            Abs_Err[k] = np.linalg.norm(Delta_y, ord=np.inf)
-            Rel_Err[k] = Abs_Err[k]/np.linalg.norm(y_star, ord=np.inf)
-            Norm_B[k] = np.linalg.norm(B, ord=np.inf)
-            y_by_iter[k,:] = y_star
-            mass[k] = h*np.sum(y_star, axis=0)
-
-            print(f"Iteration {k}, min_mass = {np.min(mass[k])}, max_mass = {np.max(mass[k])}")               
-            # y_by_iter[k,:] = y_star
-            T_by_iter[k] = T_star
-            print(f"Iteration {k}:err_abs(y)$ = {Abs_Err[k]:.3e}, T = {T_star:.5f}")
-            print(f"$|| err_rel(y) ||$ = {Rel_Err[k]:.3e}")
-            if Rel_Err[k] <= epsilon:
-                print(f"Precision reached within {k+1} iterations")
-                converged = 1
-                break
-            else: 
-                converged = 0
-
-        return k, T_by_iter, y_by_iter, Norm_B, Abs_Err, Rel_Err, converged, mass    
-
     def Newton_mass_conserv4(self,model,y0,T_0,alpha_0, Max_iter, epsilon,h=1):
         #h is the spatial step size
         #________________________________INITIALISATION_________________________________
@@ -676,7 +597,7 @@ class orbit:
     
         #______________________________Newton iteration loop________________
         for k in range(Max_iter): # Stop criterion: norm_delta_y/norm_y0: To be kept in mind for small value of y 
-            self.f = (lambda t,y: T_star*(unscaled_f(t,y) - alpha*H ))
+            self.f = (lambda t,y: T_star*(unscaled_f(t,y)) + alpha*H )
             self.Jacf = (lambda t,y: T_star*model.jacobian(t,y)) #Jacobian wrt y only
             #Solving the whole system over one period
             
@@ -684,16 +605,16 @@ class orbit:
 
             #The orthogonality phase condition s =  0 is imposed
             s = (y_star - y0)@self.f(T,y0) #unscaled_f(T,y0)
-            ds_dT = (y_star - y0)@(unscaled_f(T,y_star) -alpha*H) #Derivative wrt T
+            ds_dT = (y_star - y0)@(unscaled_f(T,y_star)) #+ alpha*H) #Derivative wrt T
             #d = (y_star - y_prev)@self.f(T_star,y_prev)/T_star
 
             ds_dy = self.f(T,y0)#unscaled_f(T,y0) #Derivative wrt y
-            ds_dalpha = -T_star*(y_star - y0)@(H) #Derivative wrt alpha
+            ds_dalpha = (y_star - y0 )@H#-T_star*(y_star - y0)@(H) #Derivative wrt alpha
             #Periodicity condition r = phi_T - y_star
             dr_dy = (monodromy - I) #Derivative wrt y
-            dr_dT = unscaled_f(T,y_star) - alpha*H #self.f(T, y_star) #Derivative wrt T
+            dr_dT = unscaled_f(T,y_star) + alpha*H #self.f(T, y_star) #Derivative wrt T
             #Derivative wrt alpha. Solving a variational equation wrt alpha
-            _, dr_dalpha = self.integ_sensitivity(y_star, S0=np.zeros(self.dim), T=T, f_param = -T_star*H)
+            _, dr_dalpha = self.integ_sensitivity(y_star, S0=np.zeros(self.dim), T=T, f_param = H)
             
             #Mass conservation condition
             Delta_m = H @ (y_star) - m0
@@ -737,6 +658,7 @@ class orbit:
             print('_________________________________________________________________________________\n')
             print(f"Iteration {k}, ")
             print(f"Mass = H@y_star = {H@y_star}")
+            print(f"Mass at time t = T: {H@phi_T}")
             print(f"alpha = {alpha:.4e}")
                         
             # y_by_iter[k,:] = y_star
@@ -780,7 +702,7 @@ class orbit:
     
         #______________________________Newton iteration loop________________
         for k in range(Max_iter): # Stop criterion: norm_delta_y/norm_y0: To be kept in mind for small value of y 
-            self.f = (lambda t,y: model.dydt(t,y) - alpha*H )
+            self.f = (lambda t,y: model.dydt(t,y) + alpha*H )
             #Solving the whole system over one period
             
             phi_T, monodromy = self.integ_monodromy(y_star,I,T_star)
@@ -791,7 +713,7 @@ class orbit:
             #d = (y_star - y_prev)@self.f(T_star,y_prev)/T_star
 
             ds_dy = model.dydt(T_star,y0)#unscaled_f(T,y0) #Derivative wrt y
-            ds_dalpha = -(y_star - y0)@(H) #Derivative wrt alpha
+            ds_dalpha = (y_star - y0)@(H) #Derivative wrt alpha
             #Periodicity condition r = phi_T - y_star
             dr_dy = (monodromy - I) #Derivative wrt y
             dr_dT = self.f(T_star,y_star) #self.f(T, y_star) #Derivative wrt T
@@ -1111,12 +1033,12 @@ class orbit:
 
         """----------------Shooting loop---------------------------"""
         for k in range(Max_iter):
-            self.f = lambda t,y: T_star*(model.dydt(t,y) - alpha*H) #because aplha will evolve.
+            self.f = lambda t,y: T_star*model.dydt(t,y) + alpha*H #because aplha will evolve.
             self.Jacf = lambda t,y: T_star*(model.jacobian(t,y) ) #Jacobian of f with respect to y
             # Step 1: Solve the ODE to get phi(T)
             phi_interp = self.ode_solver(
                 fun=self.f, t_span=[0.0, T_unit], t_eval=[T_unit], y0=y_star,
-                method="BDF", rtol=1e-7, atol=1e-9, jac=self.Jacf
+                method="RK45", rtol=1e-7, atol=1e-9, jac=self.Jacf
             )
             phi_T = phi_interp.y[:, -1].copy()
             #________________________________________________________________#
@@ -1131,7 +1053,7 @@ class orbit:
             """------Step 3: Picard correction (NPGS(l=2))-----------------"""
             
             #Moore-Spence formulation 
-            _, dr_dalpha = self.integ_sensitivity(y_star, np.zeros(self.dim), T_unit, f_param = -T_star*H)
+            _, dr_dalpha = self.integ_sensitivity(y_star, np.zeros(self.dim), T_unit, f_param = H)
             Delta_q_r = self.picard_correction(y = y_star,T = T_unit,r = phi_T-y_star, Vp=Vp,l=l)
             Delta_q_alpha = self.picard_correction(y = y_star,T = T_unit,r = dr_dalpha, Vp=Vp,l=l)
             #_________________________________________________________________#
@@ -1345,3 +1267,131 @@ class orbit:
         # phi_T, monodromy = self.integ_monodromy(y_star, I, T_star)
         return k, T_by_iter, y_by_iter, Norm_B, Abs_Err, Rel_Err, converged, mass
 
+    def Newton__mass_cont(self,model,y0,T_0,alpha_0,I_0, Max_iter, epsilon,h=1):
+        #Handling continuation in the interaction strength model.I
+        #  
+        #________________________________INITIALISATION_________________________________
+        y_star, T_star = y0.copy(), T_0
+        alpha = alpha_0 #Initial guess for the artificial variable
+        model.I = I_0
+        y_by_iter, T_by_iter = np.zeros((Max_iter,self.dim)),np.zeros((Max_iter))
+        Norm_B, Abs_Err = np.zeros((Max_iter)), np.zeros((Max_iter))
+        mass = np.zeros((Max_iter))
+        Rel_Err = np.zeros((Max_iter))
+        I = np.eye(self.dim)
+        H = h*np.ones_like(y_star)
+        m0 = H @ y0
+        T = 1.0
+        unscaled_f = model.dydt
+        
+        #Initial predictor direction
+        dy_deta = np.ones_like(y_star) #Direction of change of y with respect to the continuation parameter eta. Initially set to ones.
+        dT_deta = 1.0 #Derivative of T with respect to eta
+        dalpha_deta = 1.0 #Derivative of alpha with respect to eta
+        dI_deta  = 1.0
+        delta_eta = 1e-3 #Step size for the continuation parameter eta
+        #______________________________Newton iteration loop________________
+        for k in range(Max_iter): # Stop criterion: norm_delta_y/norm_y0: To be kept in mind for small value of y 
+            self.f = (lambda t,y: T_star*(unscaled_f(t,y)) + alpha*H )
+            self.Jacf = (lambda t,y: T_star*model.jacobian(t,y)) #Jacobian wrt y only
+
+            df_dI = lambda t,y: T_star*model.df_dI_per(t,y) #Derivative of f with respect to I
+
+            #Solving the whole system over one period here T = 1 because of the scaling.
+            
+            phi_T, monodromy = self.integ_monodromy(y_star,I,T)
+
+            #The orthogonality phase condition s =  0 is imposed
+            s = (y_star - y0)@self.f(T,y0) #unscaled_f(T,y0)
+            ds_dT = (y_star - y0)@(unscaled_f(T,y_star)) #+ alpha*H) #Derivative wrt T
+            ds_dy = self.f(T,y0)#unscaled_f(T,y0) #Derivative wrt y
+            ds_dalpha = (y_star - y0 )@H#-T_star*(y_star - y0)@(H) #Derivative wrt alpha
+            ds_dI =(y_star - y0)@df_dI(T,y0) #Derivative wrt I
+
+            #Periodicity condition r = phi_T - y_star
+            dr_dy = (monodromy - I) #Derivative wrt y
+            dr_dT = unscaled_f(T,y_star) + alpha*H #self.f(T, y_star) #Derivative wrt T
+            #Derivative wrt alpha. Solving a variational equation wrt alpha
+            _, dr_dalpha = self.integ_sensitivity(y_star, S0=np.zeros(self.dim), T=T, f_param = H)
+            _, dr_dI = self.integ_sensitivity(y_star, S0=np.zeros(self.dim), T=T, f_param = df_dI(T,y_star))
+            #Mass conservation condition
+            Delta_m = H @ (y_star) - m0
+            #c2 = H #derivative wrt y
+            dm_dT  = 0.0 #H @ self.f(T,y_star)  #0 #derivative wrt T d22 = A32
+            dm_dalpha = 0.0 #Derivative wrt alpha
+            dm_dy =  H
+            dm_dI = 0.0 #Derivative wrt I 
+
+            #Pseudo-arclength condition
+            n = (y_star - y0)@dy_deta + (T_star - T_0)*dT_deta + (alpha - alpha_0)*dalpha_deta + (I - I_0)*dI_deta - delta_eta         
+
+            dn_dy = dy_deta
+            dn_dT = dT_deta
+            dn_dalpha = dalpha_deta 
+            dn_dI = dI_deta
+
+            #Assembling the whole matrix
+            top = np.hstack((dr_dy, dr_dT.reshape(-1,1), dr_dalpha.reshape(-1,1), dr_dI.reshape(-1,1)))  # Horizontal stacking of A11=M-I, A12= dr_dT and A13 = dr_dalpha 
+            middle_1 = np.hstack((ds_dy.reshape(1,-1), np.array([[ds_dT]]), np.array([[ds_dalpha]]), np.array([[ds_dI]])))  # Horizontal stacking of A21=ds_dy, A22=ds_dT and A23= ds_dalpha
+            middle_2 = np.hstack((dn_dy.reshape(1,-1), np.array([[dn_dT]]), np.array([[dn_dalpha]]), np.array([[dn_dI]]))) #Horizontal stacking of A21=dn_dy, A22=dn_dT and A23= dn_dalpha
+            bottom = np.hstack((dm_dy.reshape(1,-1), np.array([[dm_dT]]), np.array([[dm_dalpha]]), np.array([[dm_dI]]))) #Horizontal stacking of A31, A32 and A33
+
+            Mat = np.vstack((top, middle_1, middle_2, bottom))  # Vertical stacking of the three rows
+
+            #Right hand side concatenation
+            B = np.concatenate((phi_T - y_star, np.array([s]), np.array([n]), np.array([Delta_m])))
+            
+            
+            # XX, residues,rank,sing_val = lstsq(Mat,-B,lapack_driver='gelss') #Contain Delta_X and Delta_T
+
+            XX = solve(Mat, -B) #Contain Delta_X, Delta_T and Delta_alpha
+            Delta_y = XX[:self.dim]
+            Delta_T = XX[self.dim]
+            Delta_I = XX[-2]
+            Delta_alpha = XX[-1]
+            #Updating
+            y_star += Delta_y
+            T_star += Delta_T
+            alpha += Delta_alpha
+            model.I += Delta_I
+
+            #Generating the new predictor direction for the next iteration using the current tangent vector
+            #Using the same matrix but with a different right-hand side corresponding to the tangent vector
+            B_tangent = np.concatenate((np.zeros_like(y_star), np.array([0.0]), np.array([dn_deta]), np.array([0.0]))) #Right-hand side
+            
+            #Estimation of the errors
+            Abs_Err[k] = np.linalg.norm(Delta_y, ord=np.inf)
+            Rel_Err[k] = Abs_Err[k]/np.linalg.norm(y_star, ord=np.inf)
+            Norm_B[k] = np.linalg.norm(B, ord=np.inf)
+            y_by_iter[k,:] = y_star
+            T_by_iter[k] = T_star
+            mass[k] = H@y_star  #np.abs(Delta_m) #h*np.sum(y_star, axis=0)
+            
+            print('_________________________________________________________________________________\n')
+            print(f"Iteration {k}, ")
+            print(f"Mass = H@y_star = {H@y_star}")
+            print(f"Mass at time t = T: {H@phi_T}")
+            print(f"alpha = {alpha:.4e}")
+                        
+            # y_by_iter[k,:] = y_star
+            
+            print(f"||err_abs(y)|| = {Abs_Err[k]:.3e}, T = {T_star:.5f}")
+            print(f"$||err_rel(y)||$ = {Rel_Err[k]:.4e}")
+            if Rel_Err[k] <= epsilon:
+                print(f"Precision reached within {k+1} iterations")
+                converged = 1
+                break
+            # Preventing explosion of the variables
+            elif Abs_Err[k] >= 1e2:
+                print("Abs_Err too large, stopping iteration: Divergence.")
+                converged = -1
+                break
+            elif T_star <= 0:
+                print("Negative period, stopping iteration: Divergence.")
+                converged = -1
+                break
+            elif k >= Max_iter-1:
+                converged = 0
+                print("Maximum number of iterations reached.")
+
+        return k, T_by_iter, y_by_iter, Norm_B, Abs_Err, Rel_Err, converged, mass